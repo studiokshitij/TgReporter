5696e9536d82c5e494afcba144f4523f8067c7a7
studiokshitij <kshitijshresth@myyahoo.com>
2026-01-13 17:31:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+TOKEN = "bot_token"
+
+
+bot_name = "Safest Gram Bot"
+bot_logs = -1003557266197  
+bot_admin = "@Kshitijshreshth"
+bot_banner_link = "https://i.ibb.co/vvq2sQSk/IMG-20260105-190751-513.jpg"
+
+
+CRYPTO = "44444:gggg"
+
+
+ADMINS = [414932992]  
+
+
+subscribe_1_day = 1
+subscribe_7_days = 7
+subscribe_14_days = 14
+subscribe_30_days = 18
+subscribe_365_days = 120
+subscribe_infinity_days = 180
